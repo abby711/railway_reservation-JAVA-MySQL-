e9ad46307e99f90eca218dd7c23498ede04beb60
Abirami <65455645+abby711@users.noreply.github.com>
2020-07-02 13:20:23 +0530

    Add files via upload

---
+++ rail/src/rail/info.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rail;
+
+/**
+ *
+ * @author Abirami
+ */
+public class info {
+    public static String from;
+     public static String tno;
+    public static String to;
+    public static String clas;
+    //public static int d;
+    public static String date;
+    //public static int y;
+    public static String seat;
+    public static String child,adult,old,bookid;
+    public static float cost;
+    
+    
+}
